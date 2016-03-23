c2ad1616ee7873ac9f0ddfc326d041a41e0040a0
Matheus Faria <mths.faria@hotmail.com>
2016-03-23 20:41:34 -0300

    Push the 21 lines python code.

---
+++ trackip.py
@@ -0,0 +1,24 @@
+import urllib2
+import json
+import iptools
+import sys
+
+ip = sys.argv[1]
+city = sys.argv[2]
+
+while(True):
+
+	response = urllib2.urlopen('http://ip-api.com/json/' + ip)
+	data = json.load(response)
+
+	print data['query'] + ' - ' + data['city'] + ' - ' + data['org'] + ' - ' + data['as']
+
+	if data['city'] == city:
+		with open('bh_ips.txt', 'a') as ipsFile:
+			ipsFile.write(ip)
+
+	ip = iptools.ipv4.ip2long(ip) + 1
+	ip = str(iptools.ipv4.long2ip(ip))
+
+# Usage: sudo pip install iptools
+# python trackip.py ip city
